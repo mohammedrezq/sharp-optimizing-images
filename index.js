a64408e75850f716fd5ba3d57b05fb43f33bb571
Mohammed Rezq <mohammedrezq2000@gmail.com>
2022-10-01 15:06:44 +0200

    [Sharp-1] improvements

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ fileNames.forEach((file) => {
 
   let sh = sharp("./images/" + file);
 
-  if(fileFormat == 'jpg') {
+  if( 'jpg' == fileFormat ) {
     sh.jpeg({
       quality: 80,
       progressive: true,
@@ -28,7 +28,7 @@ fileNames.forEach((file) => {
         fit:  sharp.fit.contain,
         width: 900,
       }
-    ).flip(true)
+    )
   } else if ('png' === fileFormat) {
     sh.png({
       quality: 80,
@@ -39,26 +39,12 @@ fileNames.forEach((file) => {
         width: 900,
       }
     )
+  } else if('webp' === fileFormat){
+    sh.webp({lossless:true, quality: 80, alphaQuality: 100, force: true}).resize(
+        {fit: sharp.fit.contain, width: 900}
+      )
   }
 
-  // sh.png({
-    
-  // })
-  // sh.webp({lossless:true, quality: 80, alphaQuality: 100, force: true}).resize(
-  //   {fit: sharp.fit.contain, width: 700}
-  // )
-
-  // if (fileFormat === "jpg" || fileFormat === "jpeg") {
-  //   sh = sh.jpeg({ quality: 70 }).resize({
-  //     width: 350,
-  //     height: 450,
-  //   });
-  // } else if (fileFormat === "png") {
-  //   sh = sh.png({ quality: 70 }).resize({
-  //     width: 350,
-  //     height: 450,
-  //   });
-  // }
 
   console.log('output/' + file);
   let fileNameWOExtension = file.replace(/\.[^/.]+$/, "");
@@ -79,18 +65,3 @@ function getExtension(filename) {
 
   return ext[ext.length - 1];
 }
-
-// async function optimizeImage() {
-//   try {
-//     const image = await sharp("./images/university.jpg")
-//       .resize({ width: 6500, height: 4800 })
-//       .webp({
-//         quality: 10,
-//       })
-//       .toFile("./output/gfddsg4500.webp");
-//   } catch (error) {
-//     console.log(error);
-//   }
-// }
-
-// optimizeImage();
